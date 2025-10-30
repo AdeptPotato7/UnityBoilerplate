f1b80e92f1e1e152b2fbb3aff54fba71258adafe
Larry Davis <cobblestone1007@gmail.com>
2025-10-29 20:38:56 -0400

    Vs studio Stuff

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
    public void Play()
    {
         UnityEngine.SceneManagement.SceneManager.LoadScene("DaGame");
