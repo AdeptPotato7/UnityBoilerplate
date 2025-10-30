4dc621d42fa253001cfd565549effc663c210c10
Larry Davis <cobblestone1007@gmail.com>
2025-10-29 20:22:06 -0400

    Pause Menu Bug
    I fixed the issue I had with the other game by setting the time scale back to one in the main menu in void update

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
    public void Play()
-    {
+   {
         UnityEngine.SceneManagement.SceneManager.LoadScene("DaGame");
-    }
+   }
+
     public void Quit()
     {
         Application.Quit();
     }
+
+   void Update()
+   {
+        Time.timeScale = 1;
+   }
 }
