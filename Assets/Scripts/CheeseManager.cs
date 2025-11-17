6b329268c4fc7e8ef2f08f900a014874997cb48d
Larry Davis <cobblestone1007@gmail.com>
2025-11-17 17:15:00 -0500

    Cheese Counter

--- Assets/Scripts/CheeseManager.cs
+++ Assets/Scripts/CheeseManager.cs
@@ -1,14 +1,24 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class CheeseManager : MonoBehaviour
 {
+    public static CheeseManager instance;
+    public TextMeshProUGUI cheeseText;
     static int cheese = 0;
     public int cheeseToWin = 18;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
     void Start()
     {
         cheese = 0;
+        UpdateCheeseDisplay();
     }
 
     public int getCheese() { return cheese; }
@@ -16,14 +26,25 @@ public class CheeseManager : MonoBehaviour
     public void addCheese(int amount)
     { 
         cheese += amount;
+        UpdateCheeseDisplay();
+
         if (cheese >= cheeseToWin)
         {
             WinGame();
         }
     }
 
+    void UpdateCheeseDisplay()
+    {
+        cheeseText.text = "Cheese: " + cheese + "/" + cheeseToWin;
+    }
    void WinGame()
    { 
                UnityEngine.SceneManagement.SceneManager.LoadScene("StartScreen");   
    }
+
+    public int GetCheeseCount()
+    {
+        return cheese;
+    }
 }
